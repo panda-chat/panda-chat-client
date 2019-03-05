b870374d2064eb25dfb15f859c9862e91bd644e4
Alex <alexandercstephens@gmail.com>
2019-03-05 16:55:09 -0600

    Separate dev and prod settings in webpack
    Replace settings.ts with a section in webpack.config.js that changes depending on which mode webpack is run in, and update `npm run dev` and `npm run build` to run in the appropriate modes. The `npm run watch` command might needs its mode set, but I don't know what that command is being used for, or even if it's still being used.
    
    There are probably other settings in webpack.config.js that could be for development mode only (e.g. devServer), but I didn't mess with those for now.

--- app/src/settings.ts
+++
@@ -1,4 +0,0 @@
-export const settings = {
-    messagesEndpoint: "https://api.pandachat.cc/messages/?quantity=100" as string,
-    websocketEndpoint: "wss://api.pandachat.cc/ws/" as string
-}
--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,84 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: ['./app/src/draw.ts', './app/site.scss'],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    watchContentBase: true,
-    open: true,
-    publicPath: '/',
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [ 
-          "style-loader",
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader"
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: 'img/[name].[ext]',
-          },
-        }],
-      },
-    ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      {from: path.join(__dirname, 'app/robots.txt'), to: 'robots.txt'}
-    ]),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'app/index.html'),
-      filename: 'index.html',
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  resolve: {
-    extensions: ['.ts', '.js', '.scss']
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+module.exports = (env, argv) => {
+
+  // Settings for both development and production.
+  exports = {
+    entry: ['./app/src/draw.ts', './app/site.scss'],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      watchContentBase: true,
+      open: true,
+      publicPath: '/',
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [ 
+            "style-loader",
+            "css-loader",
+            "postcss-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: "html-loader"
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: 'img/[name].[ext]',
+            },
+          }],
+        },
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        {from: path.join(__dirname, 'app/robots.txt'), to: 'robots.txt'}
+      ]),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'app/index.html'),
+        filename: 'index.html',
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: {
+      extensions: ['.ts', '.js', '.scss']
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    }
   }
+
+  // Settings for just development.
+  if (argv.mode == 'development') {
+    exports.plugins.push(new webpack.DefinePlugin({
+      "settings.messagesEndpoint": "'http://localhost:4000/messages/?quantity=100'",
+      "settings.websocketEndpoint": "'ws://localhost:4000/ws/'",
+    }));
+  }
+
+  // Settings for just production.
+  if (argv.mode == 'production') {
+    exports.plugins.push(new webpack.DefinePlugin({
+      "settings.messagesEndpoint": "'https://api.pandachat.cc/messages/?quantity=100'",
+      "settings.websocketEndpoint": "'wss://api.pandachat.cc/ws/'",
+    }));
+  }
+
+  return exports;
 };
